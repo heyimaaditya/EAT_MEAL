e50bc7dede1a33ddb5348297b46828ae7079dea6
heyimaaditya <aadityatyagi158@gmail.com>
2025-02-10 22:50:25 +0530

    Routes for password reset done

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const router=express.Router();
-const {login,signup}=require("../controllers/authController");
+const {login,signup,changePassword,resetPassword,resetPasswordToken}=require("../controllers/authController");
 router.post('/signup',signup);
 router.post('/login',login);
+router.post('/changePassword',changePassword);
+router.post('/reset-Password',resetPassword)
+router.post('/reset-Password-Token',resetPasswordToken);
 module.exports=router;
